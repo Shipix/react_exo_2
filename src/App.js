600461c7b0ff8e4a8fa994e58d2777b5a2bf3a54
Shipix <shipix@hotmail.fr>
2021-01-14 01:28:09 +0100

    partie 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import ImgA from './logoshipix.png';
+import ImgB from './shipixjapan.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ImgA} alt="monLogo"height="300px"width="250px"/>
+        <img src={ImgB} alt="monLogo"height="300px"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Testo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ImageA extends Component {
+    render() {
+        return (
+            <div>
+                <p>oklm</p>
+                <img src={"../public/shipixloogo.png"} alt=""/>
+            </div>
+        )
+    }
+}
